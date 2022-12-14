a7420cb5d3a78a4b9e0faaf7d1533d8235d9a285
Piletskii-Oleg <piletskii.oleg@gmail.com>
2022-12-14 13:00:16 +0300

    Implement Network Chat

---
+++ NetworkChat/NetworkChat/Client.cs
@@ -0,0 +1,61 @@
+﻿namespace NetworkChat;
+
+using System.Net.Sockets;
+
+public class Client
+{
+    private TcpClient client;
+    private NetworkStream stream;
+    private StreamWriter writer;
+
+    private CancellationTokenSource tokenSource;
+    
+    public bool IsConnectionClosed { get; private set; }
+
+    public Client(string uri, int port)
+    {
+        client = new TcpClient(uri, port);
+        stream = client.GetStream();
+        writer = new StreamWriter(stream);
+        
+        tokenSource = new();
+    }
+
+    public async Task Send(string message)
+    {
+        await writer.WriteLineAsync(message);
+        await writer.FlushAsync();
+
+        if (message == "exit" || tokenSource.IsCancellationRequested)
+        {
+            Close();
+        }
+    }
+
+    public async Task<string> Receive()
+    {
+        var reader = new StreamReader(stream);
+        
+        var receivedMessage = await reader.ReadLineAsync();
+        if (receivedMessage == null)
+        {
+            throw new InvalidOperationException();
+        }
+        
+        if (receivedMessage == "exit" || tokenSource.IsCancellationRequested)
+        {
+            Close();
+        }
+
+        return receivedMessage;
+    }
+
+    public void Close()
+    {
+        client.Close();
+        stream.Close();
+        writer.Close();
+
+        IsConnectionClosed = true;
+    }
+}
---
+++ NetworkChat/NetworkChat/Program.cs
@@ -0,0 +1,51 @@
+﻿using NetworkChat;
+
+string endString = "exit";
+
+if (args.Length == 1)
+{
+    var server = new Server(int.Parse(args[0]));
+    await server.Start();
+    
+    var sendTask = Task.Run(async () =>
+    {
+        while (!server.IsClosed)
+        {
+            var message = Console.ReadLine();
+            await server.Send(message);
+        }
+    });
+
+    var receiveTask = Task.Run(async () =>
+    {
+        while (!server.IsClosed)
+        {
+            Console.WriteLine(await server.Receive());
+        }
+    });
+
+    Task.WaitAny(sendTask, receiveTask);
+}
+else if (args.Length == 2)
+{
+    var client = new Client(args[0], int.Parse(args[1]));
+
+    var sendTask = Task.Run(async () =>
+    {
+        while (!client.IsConnectionClosed)
+        {
+            var message = Console.ReadLine();
+            await client.Send(message);
+        }
+    });
+    
+    var receiveTask = Task.Run(async () =>
+    {
+        while (!client.IsConnectionClosed)
+        {
+            Console.WriteLine(await client.Receive());
+        }
+    });
+
+    Task.WaitAny(sendTask, receiveTask);
+}
